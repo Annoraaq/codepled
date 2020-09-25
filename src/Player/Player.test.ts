97b15c60a7157d91b44f15900582184ebccc1a0b
Johannes Baum <baum@bw-webgestaltung.de>
2020-09-25 12:15:17 +0200

    #19 introduce PlayerUi

--- src/Player/Player.test.ts
+++ src/Player/Player.test.ts
@@ -1,3 +1,34 @@
+import { Command, CommandType } from "./../DiffConverter/Commands";
+import { Player } from "./Player";
+
 describe("Player", () => {
-  it("should set current command index", () => {});
+  let player: Player;
+  let playerUiMock = {
+    init: jest.fn(),
+    setInitialText: jest.fn(),
+    addCommands: jest.fn(),
+  };
+
+  beforeEach(() => {
+    playerUiMock.init.mockReset();
+    playerUiMock.setInitialText.mockReset();
+    playerUiMock.addCommands.mockReset();
+    player = new Player(<any>playerUiMock);
+  });
+
+  it("should init", () => {
+    player.init();
+    expect(playerUiMock.init).toHaveBeenCalledTimes(1);
+  });
+
+  it("should set initil text", () => {
+    player.setInitialText("initial text");
+    expect(playerUiMock.setInitialText).toHaveBeenCalledWith("initial text");
+  });
+
+  it("should add commands", () => {
+    const commands: Command[] = [[CommandType.DELETE, 1]];
+    player.addCommands(commands);
+    expect(playerUiMock.addCommands).toHaveBeenCalledWith(commands);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Player } from "./Player/Player";
 import { displayText } from "./displayText";
 import { diff_match_patch as DiffMatchPatch } from "diff-match-patch";
 import { input } from "./input";
 import { transformed } from "./transformed";
 import { DiffConverter } from "./DiffConverter/DiffConverter";
+import { Player } from "./Player/Player";
 const dmp = new DiffMatchPatch();
 const diff = dmp.diff_main(input, transformed);
 const diffConverter = new DiffConverter();
