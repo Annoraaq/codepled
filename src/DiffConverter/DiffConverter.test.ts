536e5914c4b064d0cd903e525de2cae6938161a6
Johannes Baum <baum@bw-webgestaltung.de>
2020-10-24 09:17:20 +0200

    #30: refactor

--- src/DiffConverter/DiffConverter.test.ts
+++ src/DiffConverter/DiffConverter.test.ts
@@ -6,32 +6,13 @@ describe("DiffConverter", () => {
   beforeEach(() => {
     diffConverter = new DiffConverter();
   });
-  it("converts deletes", () => {
-    const diff: Diff = [CommandType.DELETE, "hello"];
-    const expectedCommands: Command[] = [
-      [CommandType.DELETE, 1],
-      [CommandType.DELETE, 1],
-      [CommandType.DELETE, 1],
-      [CommandType.DELETE, 1],
-      [CommandType.DELETE, 1],
-    ];
-    expect(diffConverter.createCommands([diff])).toEqual(expectedCommands);
-  });
 
-  it("converts skips", () => {
+  it("converts skips for fast forward", () => {
     const diff: Diff = [CommandType.SKIP, "hello"];
     const expectedCommands: Command[] = [[CommandType.SKIP, 5]];
-    expect(diffConverter.createCommands([diff])).toEqual(expectedCommands);
-  });
-
-  it("converts inserts", () => {
-    const diff: Diff = [CommandType.INSERT, "asd"];
-    const expectedCommands: Command[] = [
-      [CommandType.INSERT, "a"],
-      [CommandType.INSERT, "s"],
-      [CommandType.INSERT, "d"],
-    ];
-    expect(diffConverter.createCommands([diff])).toEqual(expectedCommands);
+    expect(diffConverter.createCommandsFastForward([diff])).toEqual(
+      expectedCommands
+    );
   });
 
   it("should merge inserts for fast forward", () => {
--- src/DiffConverter/DiffConverter.ts
+++ src/DiffConverter/DiffConverter.ts
@@ -3,26 +3,6 @@ import { Command, CommandType } from "./Commands";
 export type Diff = [CommandType, string];
 
 export class DiffConverter {
-  createCommands(diff: Diff[]): Command[] {
-    const commands: Command[] = [];
-    diff.forEach(([commandType, commandPayload]: Diff) => {
-      switch (commandType) {
-        case CommandType.DELETE:
-          commands.push(...this.transformDelete(commandPayload));
-          break;
-        case CommandType.SKIP:
-          commands.push([CommandType.SKIP, commandPayload.length]);
-          break;
-        case CommandType.INSERT:
-          for (let i = 0; i < commandPayload.length; i++) {
-            commands.push([CommandType.INSERT, commandPayload[i]]);
-          }
-          break;
-      }
-    });
-    return commands;
-  }
-
   createCommandsFastForward(diff: Diff[]): Command[] {
     const commands: Command[] = [];
     diff.forEach(([commandType, commandPayload]: Diff) => {
@@ -38,14 +18,6 @@ export class DiffConverter {
           break;
       }
     });
-    return commands;
-  }
-
-  private transformDelete(commandPayload: string) {
-    const commands: Command[] = [];
-    for (let i = 0; i < commandPayload.length; i++) {
-      commands.push([CommandType.DELETE, 1]);
-    }
     return commands;
   }
 }
